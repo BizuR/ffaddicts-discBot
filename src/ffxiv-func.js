6a3cf75364adc253ecc9a876354910feef7143d3
KuriKuri-Chan <prunier.christophe79@gmail.com>
2019-01-02 02:27:09 +0100

    Ajout stats et class

--- src/ffxiv-func.js
+++ src/ffxiv-func.js
@@ -2,13 +2,18 @@ const PropertiesReader = require('properties-reader');
 const properties = PropertiesReader(process.argv[2]);
 const xivapi_key = properties.get("xivapi.secret.key");
 const whois = require('./whois');
+const stats = require('./stats');
 
 module.exports = {
     ffwhois: function(msg) {
        whois(msg, xivapi_key);
     },
 
+    ffstats: function(msg) {
+        stats(msg, xivapi_key);
+    },
     fftest: function(msg){
         msg.reply('Ca fonctionne !');
     }
+
   };
